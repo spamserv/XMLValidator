3d3d5bfb1bad17df263016c943a68cd51fcd89f5
Kristijan Šimenić <ksimenic1@gmail.com>
2016-01-14 17:35:27 +0100

    Latest working copy

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function check(xml) {
 			popped_tag = attribute_stack.pop();
 
 			if(tag_name != popped_tag) {
-				error_stack.push("Tag : '" + popped_tag + "' nije nikada zatvoren.");
+				error_stack.push("Očekuje se zatvaranje taga : '</" + popped_tag + ">'.");
 			}
 			anything = true;
 		}
@@ -80,7 +80,7 @@ function check(xml) {
 			anything = true;
 
 		if(!anything) {
-			error_stack.push("Tag: '"+line+"' ne odgovara niti jednoj od mogućih opcija.");
+			error_stack.push("Tag: '"+line+"' nije ispravna sintaksa xml taga.");
 		}
 
 	});
@@ -94,6 +94,8 @@ function check(xml) {
 		error += attribute_stack.length == 1 ? " nije nikada zatvoren." : " nisu nikada zatvoreni.";
 		error_stack.push(error);
 	}
+	
+	
 
 	//output errors
 	panel = $("#debug-panel");
@@ -107,7 +109,7 @@ function check(xml) {
 		});
 	} else {
 		panel.removeClass('panel-danger').addClass('panel-success');
-		output.val("No errors found.");
+		output.val("Ispravan XML.");
 	}
 	
 
