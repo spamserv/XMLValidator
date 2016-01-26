92adc5828a071b433acf94681f2b7033a817a2f5
Kristijan Šimenić <ksimenic1@gmail.com>
2016-01-26 19:55:46 +0100

    Renamed everything on croatian language, removed error stack, changed error output logic

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 	pocetak_taga = /<[a-zA-Z_][a-zA-Z0-9_-]*( [a-zA-Z_][a-zA-Z0-9_-]*\=\"[a-zA-Z0-9_-]*\")*>/;
 	self_closing = /<[a-zA-Z_][a-zA-Z0-9_-]*( [a-zA-Z_][a-zA-Z0-9_-]*\=\"[a-zA-Z0-9_-]*\")*\/>/;
 	kraj_taga = /<\/[a-zA-Z_][a-zA-Z0-9_-]*>/;
-
+	
 	$("#test").on('click', function() {
 		var inputXML = $("#input_area").val();
 		text = inputXML.replace(/(\r\n|\n|\r)/gm,"");
@@ -23,33 +23,24 @@ function prepare(xml) {
 		if(xml[i+1] == "<")
 			final_xml += "\n";
 	}
-
-	final_lines = "";
-	lines = final_xml.split('\n');
-	
-	lines.forEach(function(line) {
-		if(line[0] == "<") { 
-			final_lines += line + "\n";
-		}
-	});
 	check(final_xml);
 }
 
 function check(xml) {
 	line = xml.split('\n');
-	has_prolog = false;
-	attribute_stack = [];
-	error_stack = [];
-	
-	output = $("#output_area");
 
+	/* Globalne varijable */
+	attribute_stack = [];
+	has_prolog = false;
+	error_exist = false;
 	tag_opened = false;
 
+	/* Prolazak kroz pripremljeni xml */
 	for (var i = 0; i < line.length; i++) 
 	{	
-		if(error_stack.length > 0) {
+		/* Zaustavljanje for petlje */
+		if(error_exist) {
 			i=line.length;
-			attribute_stack = [];
 			continue;
 		}
 
@@ -57,7 +48,9 @@ function check(xml) {
 
 			if(i+1 < line.length) {
 				if(!kraj_taga.test(line[i+1])) {
-					error_stack.push("Očekuje se zatvaranje taga: \r\n"+line[i-1]+"\n");
+					tag_name = line[i-1].substring(1,line[i+1].indexOf('>')+1);
+					output("Očekuje se zatvaranje taga: "+line[i-1], true);
+					error_exist = true;
 				} else {
 					tag_name = line[i+1].substring(2,line[i+1].indexOf('>'));
 					popped_tag = attribute_stack.pop();
@@ -74,15 +67,19 @@ function check(xml) {
 			continue;
 		
 		if(!has_prolog) {
-			if(!prolog.test(line[i]) && !pocetak_taga.test(line[i]))
-				error_stack.push("Pogreška u prologu.");
+			if(!prolog.test(line[i]) && !pocetak_taga.test(line[i])) {
+				output("Pogreška u prologu.", true);
+				error_exist = true;
+			}
 			has_prolog = true;
 			anything = true;
 		}
 		
 		if(pocetak_taga.test(line[i])) {
-			if(nedozvoljena_sintaksa.test(line[i]))
-				error_stack.push("Nevaljana sintaksa (pojava ključne riječi XML kao tag name ili property name");
+			if(nedozvoljena_sintaksa.test(line[i])) {
+				output("Nevaljana sintaksa (pojava ključne riječi XML kao tag name ili property name.", true);
+				error_exist = true;
+			}
 			if(line[i].indexOf(' ') == -1) {
 				tag_name = line[i].substring(1,line[i].indexOf('>'));
 			} else {
@@ -111,7 +108,8 @@ function check(xml) {
 						final_close+='<span class="bad">'+((tag_name[i] != undefined) ? tag_name[i] : '')+'</span>';
 					}
 				}
-				error_stack.push("<div>Očekuje se zatvaranje taga: <br> "+final_open+"<br>"+final_close+"<br></div>");
+				output("Očekuje se zatvaranje taga: <br> "+final_open+"<br>"+final_close+"<br>", false);
+				error_exist = true;
 			}
 			anything = true;
 			tag_opened = false;
@@ -121,36 +119,28 @@ function check(xml) {
 			anything = true;
 
 		if(!anything) {
-			error_stack.push("Tag: '"+line[i]+"' nije ispravna sintaksa xml taga.");
+			output("Tag: '"+line[i]+"' nije ispravna sintaksa xml taga.", true);
+			error_exist = true;
 		}
 
 	}
 
-	if(attribute_stack.length != 0) {
-		error = attribute_stack.length == 1 ? "Tag: " : "Tagovi: ";
-		attribute_stack.forEach(function(er) {
-			error += "'" + er + "', ";
-		});
-		error = error.substring(0,error.length-2);
-		error += attribute_stack.length == 1 ? " nije nikada zatvoren." : " nisu nikada zatvoreni.";
-		error_stack.push(error);
+	if(!error_exist) {
+		output = $("#izlaz");
+		output.html("");
+		$("#debug-panel").removeClass('panel-danger').addClass('panel-success');
+		output.text("Ispravan XML.");
 	}
-	
-	
+}
 
-	//output errors
-	panel = $("#debug-panel");
+function output(error, text) {
 	output = $("#izlaz");
 	output.html("");
-	if(error_stack.length != 0) {
-		panel.removeClass('panel-success').addClass('panel-danger');
-		error_stack.forEach(function(el) {
-			output.html(el);
-		});
-	} else {
-		panel.removeClass('panel-danger').addClass('panel-success');
-		output.html("Ispravan XML.");
-	}
-	
+
+	$("#debug-panel").removeClass('panel-success').addClass('panel-danger');
+	if(text)
+		output.text(error);
+	else
+		output.html(error);
 
 }
