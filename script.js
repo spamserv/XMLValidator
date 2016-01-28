7f4eeea4e6df2d93e4b0aaafdc41ef0ed312fd57
Kristijan Šimenić <ksimenic1@gmail.com>
2016-01-28 13:17:41 +0100

    Renamed everything to Croatian language in logic

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $( document ).ready(function() {
 	prolog = /<\?xml(( version=\"[0-9](.[0-9])*\")| (encoding=\"[a-zA-Z_][a-zA-Z0-9_]*\")| standalone=\"(yes|no)\")*\?>/i;
 	nedozvoljena_sintaksa = /<(xml.*>|[a-zA-Z_-][a-zA-Z0-9_-]* xml="[a-zA-Z_-][a-zA-Z_-]*"( [a-zA-Z_-][a-zA-Z0-9_-]*="[a-zA-Z_-][a-zA-Z_-]*")*>)+/i;
 	komentar = /(<\!\-\-).*(\-\-\>)/;
-	pocetak_taga = /<[a-zA-Z_][a-zA-Z0-9_-]*( [a-zA-Z_][a-zA-Z0-9_-]*\=\"[a-zA-Z0-9_-]*\")*>/;
+	otvoreni_tag = /<[a-zA-Z_][a-zA-Z0-9_-]*( [a-zA-Z_][a-zA-Z0-9_-]*\=\"[a-zA-Z0-9_-]*\")*>/;
 	self_closing = /<[a-zA-Z_][a-zA-Z0-9_-]*( [a-zA-Z_][a-zA-Z0-9_-]*\=\"[a-zA-Z0-9_-]*\")*\/>/;
-	kraj_taga = /<\/[a-zA-Z_][a-zA-Z0-9_-]*>/;
+	zatvoreni_tag = /<\/[a-zA-Z_][a-zA-Z0-9_-]*>/;
 	
 	/* klik na dugme -> početak provjere, odnosno priprema dokumenta te provjera */
 	$("#test").on('click', function() {
@@ -39,162 +39,161 @@ function check(xml) {
 		line = xml.split('\n');
 
 		/* Globalne varijable */
-		attribute_stack = [];
-		prolog_checked = false;
-		error_exist = false;
-		tag_opened = false;
+		stog_atributa = [];
+		prolog_provjeren = false;
+		error_postoji = false;
+		tag_otvoren = false;
 		tekst_dozvoljen = false;
 
 		/* Prolazak kroz pripremljeni xml */
 		for (var i = 0; i < line.length; i++) 
 		{	
 			/* Zaustavljanje for petlje ukoliko postoji error */
-			if(error_exist) {
+			if(error_postoji) {
 				i=line.length;
 				continue;
 			}
 
 			/* Provjera ukoliko se iza otvorenog taga nalazi teks koji nije komentar ili otvoreni tag, te nije zatvoreni tag a mora biti da se javi greška */
-			if(tag_opened && !pocetak_taga.test(line[i]) && !(line[i].trim()==='') && !komentar.test(line[i])) {
+			if(tag_otvoren && !otvoreni_tag.test(line[i]) && !(line[i].trim()==='') && !komentar.test(line[i])) {
 
 				/* Provjera zbog errora budući da se može javiti error u posljednjoj liniji */
 				if(i+1 < line.length) {
 					/* 
 					 * Ukoliko ne postoji zatvoreni tag javimo grešku kako se treba zatvoriti otvoreni tag
 					*/
-					if(!kraj_taga.test(line[i+1])) {
-						tag_name = line[i-1].substring(1,line[i+1].indexOf('>')+1);
-						output("Očekuje se zatvaranje taga: "+line[i-1], true);
-						error_exist = true;
+					if(!zatvoreni_tag.test(line[i+1])) {
+						ispis("Očekuje se zatvaranje taga: "+line[i-1], true);
+						error_postoji = true;
 					}
 					/* Smatramo tag zatvorenim */
-					tag_opened = false;
+					tag_otvoren = false;
 				}
 			}
 			/* varijabla kojom provjeravama je li se išta dogodilo */
-			anything = false;
+			bilo_sto = false;
 			
 			/* provjeravamo prolog samo ukoliko nije do sada provjeren */
-			if(!prolog_checked) {
+			if(!prolog_provjeren) {
 				/* Ukoliko nije dobar prolog vraćamo pogrešku 
 				 * - također provjeravamo i je li to slučajno početak taga zato što prolog ne mora biti deklariran po sintaksi xml dokumenta 
 				*/
-				if(line[i][0] == '<' && !prolog.test(line[i]) && !pocetak_taga.test(line[i])) {
-					output("Pogreška u prologu.", true);
-					error_exist = true;
+				if(line[i][0] == '<' && !prolog.test(line[i]) && !otvoreni_tag.test(line[i])) {
+					ispis("Pogreška u prologu.", true);
+					error_postoji = true;
 					continue;
 				}
 				if(prolog.test(line[i])) {
-					anything = true;
+					bilo_sto = true;
 				}
-				prolog_checked = true;
+				prolog_provjeren = true;
 			}
 			
 			/* Provjeravamo je li zadana linija pocetak taga, a ukoliko jest provjeravamo je li sintaksa dozovoljena */
-			if(pocetak_taga.test(line[i])) {
+			if(otvoreni_tag.test(line[i])) {
 				if(nedozvoljena_sintaksa.test(line[i])) {
-					output("Nevaljana sintaksa (pojava ključne riječi XML kao tag name ili property name.", true);
-					error_exist = true;
+					ispis("Nevaljana sintaksa (pojava ključne riječi XML kao tag name ili property name.", true);
+					error_postoji = true;
 				}
 				/* Uzimanje imena taga i pushanje na stog */
 				if(line[i].indexOf(' ') == -1) {
-					tag_name = line[i].substring(1,line[i].indexOf('>'));
+					ime_taga = line[i].substring(1,line[i].indexOf('>'));
 				} else {
-					tag_name = line[i].substring(1,line[i].indexOf(' '));
+					ime_taga = line[i].substring(1,line[i].indexOf(' '));
 				}
-				attribute_stack.push(tag_name);
+				stog_atributa.push(ime_taga);
 
 				/* Postavljanje globalnih varijabli */
-				anything = true;
-				tag_opened = true;
+				bilo_sto = true;
+				tag_otvoren = true;
 				tekst_dozvoljen = true;
 			}
 			
 			/* Provjeravamo je li zadana linija kraj taga, a ukoliko jest provjeravamo je li sintaksa dozovoljena */
-			if(kraj_taga.test(line[i])) {
+			if(zatvoreni_tag.test(line[i])) {
 				/* Uzimanje taga sa stoga i imena trenutnog taga za usporedbu */
-				tag_name = line[i].substring(2,line[i].indexOf('>'));
-				popped_tag = attribute_stack.pop();
+				ime_taga = line[i].substring(2,line[i].indexOf('>'));
+				zadnji_tag = stog_atributa.pop();
 
-				/* Prirprema za output ukoliko je pogrešno slovo */
-				final_open = '';
-				final_close = '';
-				veci = popped_tag > tag_name ? (popped_tag.length) : (tag_name.length);
-				if(tag_name != popped_tag) {
+				if(ime_taga != zadnji_tag) {
+					/* Prirprema za ispis ukoliko je pogrešno slovo */
+					pripremljeni_otvoreni = '';
+					pripremljeni_zatvoreni = '';
+					veci = zadnji_tag > ime_taga ? (zadnji_tag.length) : (ime_taga.length);
+
 					for(i=0;i<veci;i++){
-						if(tag_name[i] == popped_tag[i]){
-							final_open+='<span class="ok">'+((popped_tag[i] != undefined) ? popped_tag[i] : '')+'</span>';
-							final_close+='<span class="ok">'+((tag_name[i] != undefined) ? tag_name[i] : '')+'</span>';
+						if(ime_taga[i] == zadnji_tag[i]){
+							pripremljeni_otvoreni+='<span class="ok">'+((zadnji_tag[i] != undefined) ? zadnji_tag[i] : '')+'</span>';
+							pripremljeni_zatvoreni+='<span class="ok">'+((ime_taga[i] != undefined) ? ime_taga[i] : '')+'</span>';
 						}else{
-							final_open+='<span class="ok">'+((popped_tag[i] != undefined) ? popped_tag[i] : '')+'</span>';
-							final_close+='<span class="bad">'+((tag_name[i] != undefined) ? tag_name[i] : '')+'</span>';
+							pripremljeni_otvoreni+='<span class="ok">'+((zadnji_tag[i] != undefined) ? zadnji_tag[i] : '')+'</span>';
+							pripremljeni_zatvoreni+='<span class="bad">'+((ime_taga[i] != undefined) ? ime_taga[i] : '')+'</span>';
 						}
 					}
-					output("Očekuje se zatvaranje taga: <br> "+final_open+"<br>"+final_close+"<br>", false);
-					error_exist = true;
+					ispis("Očekuje se zatvaranje taga: <br> "+pripremljeni_otvoreni+"<br>"+pripremljeni_zatvoreni+"<br>", false);
+					error_postoji = true;
 				}
 
 				/* Postavljanje globalnih varijabli */
-				anything = true;
-				tag_opened = false;
+				bilo_sto = true;
+				tag_otvoren = false;
 				tekst_dozvoljen = false;
 			}
 
 			/* Provjera je li linija self closing tag ili komentar, ukoliko jest program nastavlja dalje */
 			if(self_closing.test(line[i]) || komentar.test(line[i]))
-				anything = true;
+				bilo_sto = true;
 
 			/* Provjera je li zadana linija bilo što od gore, ako nije tada je to nešto nedozvoljeno */
-			if(!anything) {
+			if(!bilo_sto) {
 				/* Ukoliko je ta linija tag, sintaksa nije dozvoljena */
 				if(line[i][0] == "<") {
-					error_exist = true;
-					output("Tag: '"+line[i]+"' nije ispravna sintaksa xml taga.", true);
+					error_postoji = true;
+					ispis("Tag: '"+line[i]+"' nije ispravna sintaksa xml taga.", true);
 				}
 				/* Ako nije prazna linija i ukoliko na tom mjestu nije dozovoljeno da stoji tekst, a već je prethodno provjereno je li tag, ispisujemo grešku */
 				else if(line[i].trim() !== '' && !tekst_dozvoljen) {
-					error_exist = true;
-					output("Neispravna sintaksta: '"+line[i]+"' mora biti između oznaka.", true);
+					error_postoji = true;
+					ispis("Neispravna sintaksta: '"+line[i]+"' mora biti između oznaka.", true);
 				}
 				
 			}
 		}
 
 		/* Ukoliko nemamo errora ali imamo atributa na stogu -> krajnji tag(ovi) nije/su zatvoreni, ispišemo ih i javljamo grešku */
-		if(!error_exist && attribute_stack.length > 0) {
-			tag_to_output = "";
-			for (var i = 0; i < attribute_stack.length; i++) {
-				if(i != attribute_stack.length-1)
-					tag_to_output += attribute_stack[i] + ", ";
+		if(!error_postoji && stog_atributa.length > 0) {
+			tag_to_ispis = "";
+			for (var i = 0; i < stog_atributa.length; i++) {
+				if(i != stog_atributa.length-1)
+					tag_to_ispis += stog_atributa[i] + ", ";
 				else
-					tag_to_output += attribute_stack[i];
+					tag_to_ispis += stog_atributa[i];
 			};
-			output("Slijedeći tagovi nisu zatvoreni: "+tag_to_output,true);
-			error_exist = true;
+			ispis("Slijedeći tagovi nisu zatvoreni: "+tag_to_ispis,true);
+			error_postoji = true;
 
 		/* Ukoliko nemamo errora, javljamo da je sve OK -> ispravan XML */
-		} else if(!error_exist) {
-			output_field = $("#izlaz");
-			output_field.html("");
+		} else if(!error_postoji) {
+			polje_za_ispis = $("#izlaz");
+			polje_za_ispis.html("");
 			$("#debug-panel").removeClass('panel-danger').addClass('panel-success');
-			output_field.text("Ispravan XML.");
+			polje_za_ispis.text("Ispravan XML.");
 		}
 	/* Ispis greške ukoliko nije uneseno ništa u polje */
 	} else {
-		output("Molimo unesite xml dokument.", true);
+		ispis("Molimo unesite xml dokument.", true);
 	}
 	
 }
 
 /* Funkcija za ispisivanje grešaka */
-function output(error, text) {
-	output_field = $("#izlaz");
-	output_field.html("");
+function ispis(error, text) {
+	polje_za_ispis = $("#izlaz");
+	polje_za_ispis.html("");
 
 	$("#debug-panel").removeClass('panel-success').addClass('panel-danger');
 	if(text)
-		output_field.text(error);
+		polje_za_ispis.text(error);
 	else
-		output_field.html(error);
-
+		polje_za_ispis.html(error);
 }
